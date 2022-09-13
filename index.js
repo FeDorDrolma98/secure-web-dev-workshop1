0920b0fcdd5a134974c94ed390a5c2cb46e81c37
FeDorDrolma98 <58807436+FeDorDrolma98@users.noreply.github.com>
2022-09-13 19:35:59 +0200

    test commit, ex1 et avancée ex2

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // Invoking strict mode https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode#invoking_strict_mode
 'use strict'
+// si on est pas strict, on est sloppy
+// c'est mieux d'etre strict, sinon certaines error restent silencieuse
+// ca peut aussi permettre d'etre plus optimisé et rapide
+
+
+
+/*
+* en fait, pour que le truc de git fonctionne, il faut ouvrir tout le projet, et pas juste le .js
+* */
 
 // https://opendata.paris.fr/explore/dataset/lieux-de-tournage-a-paris/information
-const filmingLocations = require('./lieux-de-tournage-a-paris.json')
+const filmingLocations = require('./lieux-de-tournage-a-paris.json') // juste une ligne du tableau quand filmingLocations
 
 console.log('🚀 It Works!');
 
@@ -12,20 +21,53 @@ console.log('🚀 It Works!');
  * Good luck, have fun !
  */
 
+//node fct en assynchrone, si on part sur qqch, commence deja a faire
+// la suite. du coup, comme on peut faire des callback (qqch.then(qqch.then...)...
+// du coup on arrive dans "l'enfer des callback", on est perdu
+// du coup pour eviter ca, on peut faire des await (qqch= await qqch.then...)
+// mais du coup attend, le code devient bloquant
+
+
 // 📝 TODO: Number of filming locations
 // 1. Make the function return the number of filming locations
 function getFilmingLocationsNumber () {
-	return ''
+	return filmingLocations.length
 }
 console.log(`There is ${getFilmingLocationsNumber()} filming locations in Paris`)
-
+console.log(filmingLocations[0]["fields"]["coord_y"])
 // 📝 TODO: Filming locations sorted by start date, from most recent to oldest.
 // 1. Implement the function
 // 2. Log the first and last item in array
+
+/*
+for(let i=0;i<filmingLocations.length;i++){
+	console.log(filmingLocations[i]["fields"]["nom_tournage"])
+}*/
+
 function sortFilmingLocationsByStartDate () {
-	return ''
+	console.log(filmingLocations[0]["fields"]["nom_tournage"])
+	console.log(filmingLocations[filmingLocations.length-1]["fields"]["nom_tournage"])
+	filmingLocations.sort(filmingLocations["field"]["date_debut"])
+
+	return filmingLocations[0]["fields"]["nom_tournage"]
 }
-console.log(``)
+console.log(filmingLocations[0]["fields"]["nom_tournage"])
+
+
+// POUR COMPARER
+let arrayOfIds=[{id:1},{id:2},{id:3},{id:4},{id:5}]
+const compareFn=(elementA,elementB) => elementB.id-elementA.id
+//les ligatures apparaissent et c'est plus joli
+const sortedArray=arrayOfIds.sort(compareFn)
+console.table(sortedArray)
+//console.table permet d'afficher des table. mais log s'arrete au bout de 30 genre, et table fait tout (10000 par ex)
+//
+
+
+// js a un format date
+//new Date(value:'2022-09-13T00:00:00+02:00')
+
+
 
 // 📝 TODO: Number of filming locations in 2020 only
 // 1. Make the function return the number of filming locations in 2020 only
